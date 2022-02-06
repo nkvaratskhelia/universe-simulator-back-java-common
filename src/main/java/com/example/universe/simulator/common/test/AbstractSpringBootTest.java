59cc65cd2f1bb3ca4681d8553952aca3c53de9af
nkvaratskhelia <nkvar12@gmail.com>
2022-02-06 10:37:32 +0400

    added common test annotations - added AbstractTest - added AbstractSpringBootTest - added spring-boot-starter-test dependency - updated version to 0.6.3

---
+++ src/main/java/com/example/universe/simulator/common/test/AbstractSpringBootTest.java
@@ -0,0 +1,14 @@
+package com.example.universe.simulator.common.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@AbstractTest
+@SpringBootTest
+public @interface AbstractSpringBootTest {}
---
+++ src/main/java/com/example/universe/simulator/common/test/AbstractTest.java
@@ -0,0 +1,13 @@
+package com.example.universe.simulator.common.test;
+
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+public @interface AbstractTest {}
