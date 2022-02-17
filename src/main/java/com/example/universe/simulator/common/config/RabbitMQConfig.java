420bf14502f5126011be4585efe087a2bba17400
nkvaratskhelia <nkvar12@gmail.com>
2022-02-17 17:04:47 +0400

    added rabbitmq declarations - updated version to 0.6.9 - replaced exchange bean with declarables in RabbitMQConfig

--- src/main/java/com/example/universe/simulator/common/config/RabbitMQConfig.java
+++ src/main/java/com/example/universe/simulator/common/config/RabbitMQConfig.java
@@ -1,8 +1,11 @@
 package com.example.universe.simulator.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Declarables;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.ExchangeBuilder;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -17,9 +20,20 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    TopicExchange exchange(@Value("${spring.rabbitmq.template.exchange}") String name) {
-        return ExchangeBuilder
-            .topicExchange(name)
+    public Declarables declarables(@Value("${spring.rabbitmq.template.exchange}") String exchangeName,
+                                   @Value("${app.rabbitmq.event-queue}") String eventQueueName) {
+        DirectExchange exchange = ExchangeBuilder
+            .directExchange(exchangeName)
             .build();
+
+        var eventQueue = QueueBuilder
+            .durable(eventQueueName)
+            .build();
+        var eventQueueBinding = BindingBuilder
+            .bind(eventQueue)
+            .to(exchange)
+            .with(eventQueueName);
+
+        return new Declarables(exchange, eventQueue, eventQueueBinding);
     }
 }
