fdf9f1e4a82771a2a405ffe5523957cf95689de3
nkvaratskhelia <nkvar12@gmail.com>
2022-06-04 12:16:12 +0400

    removed separate RabbitMQProperties - moved RabbitMQProperties to CommonProperties - removed RabbitMQProperties - updated version to 1.1.1

--- src/main/java/com/example/universe/simulator/common/config/CommonProperties.java
+++ src/main/java/com/example/universe/simulator/common/config/CommonProperties.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
 @ConfigurationProperties(prefix = "app")
@@ -17,4 +18,17 @@ public class CommonProperties {
      */
     @NotBlank
     private String logstashUrl;
+
+    @Valid
+    private RabbitMQProperties rabbitmq = new RabbitMQProperties();
+
+    @Getter @Setter
+    public static class RabbitMQProperties {
+
+        /**
+         * Name of the event queue.
+         */
+        @NotBlank
+        private String eventQueue;
+    }
 }
