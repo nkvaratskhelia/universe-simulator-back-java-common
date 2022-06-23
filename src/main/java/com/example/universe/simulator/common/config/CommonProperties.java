2858f5332f0c1c0081aaa4e1a3f815ce6064b0b7
nkvaratskhelia <nkvar12@gmail.com>
2022-06-23 22:12:15 +0400

    removed rabbit props from common consul properties - removed rabbit props in consul section in project-setup.adoc - removed validation in CommonProperties - updated version to 1.3.2

--- src/main/java/com/example/universe/simulator/common/config/CommonProperties.java
+++ src/main/java/com/example/universe/simulator/common/config/CommonProperties.java
@@ -3,18 +3,13 @@ package com.example.universe.simulator.common.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
-
-import javax.validation.constraints.NotBlank;
 
 @ConfigurationProperties(prefix = "app")
-@Validated
 @Getter @Setter
 public class CommonProperties {
 
     /**
      * Logstash url in the form of host:port.
      */
-    @NotBlank
     private String logstashUrl;
 }
