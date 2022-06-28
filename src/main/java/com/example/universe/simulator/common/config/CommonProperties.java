85315271257f6a97fed7316e301fbbfd80178599
nkvaratskhelia <nkvar12@gmail.com>
2022-06-28 08:17:53 +0400

    made properties injectable - added @Component in CommonProperties & RabbitMQProperties - added ignoreInvalidFields = true flag to @ConfigurationProperties in CommonProperties & RabbitMQProperties - added @Validated in CommonProperties - added @NotBlank to logstashUrl field in CommonProperties - updated version to 1.3.4

--- src/main/java/com/example/universe/simulator/common/config/CommonProperties.java
+++ src/main/java/com/example/universe/simulator/common/config/CommonProperties.java
@@ -3,13 +3,20 @@ package com.example.universe.simulator.common.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
-@ConfigurationProperties(prefix = "app")
+import javax.validation.constraints.NotBlank;
+
+@ConfigurationProperties(prefix = "app", ignoreInvalidFields = true)
+@Component
+@Validated
 @Getter @Setter
 public class CommonProperties {
 
     /**
      * Logstash url in the form of host:port.
      */
+    @NotBlank
     private String logstashUrl;
 }
