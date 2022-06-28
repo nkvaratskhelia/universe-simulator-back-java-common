da1b4b41dee866eefbdb2b7168be9e9f75b5bb86
nkvaratskhelia <nkvar12@gmail.com>
2022-06-28 09:30:17 +0400

    made configuration props readonly - removed @Setter in CommonProperties & RabbitMQProperties - added @AllArgsConstructor in CommonProperties & RabbitMQProperties - added @ConstructorBinding in CommonProperties & RabbitMQProperties - removed @Component in CommonProperties & RabbitMQProperties - reordered lombok and spring-boot-configuration-processor dependencies - updated common version to 1.3.6

--- src/main/java/com/example/universe/simulator/common/config/CommonProperties.java
+++ src/main/java/com/example/universe/simulator/common/config/CommonProperties.java
@@ -1,13 +1,14 @@
 package com.example.universe.simulator.common.config;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
 @ConfigurationProperties(prefix = "app")
-@Component
-@Getter @Setter
+@ConstructorBinding
+@AllArgsConstructor
+@Getter
 public class CommonProperties {
 
     /**
