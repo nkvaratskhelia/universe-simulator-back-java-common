9023bbac15f9e091da967b4d25b5c1a0a67904ac
nkvaratskhelia <nkvar12@gmail.com>
2022-11-26 09:36:47 +0400

    updated spring boot & gradle - updated gradle to 7.6 - updated spring boot to 3.0.0 - updated sonarqube plugin to 3.5.0.2730 - updated version to 1.7.2 - updated consul to 1.14.1 - removed @ConstructorBinding in properties classes - replaced javax validation with jakarta validation in RabbitMQProperties

--- src/main/java/com/example/universe/simulator/common/config/CommonProperties.java
+++ src/main/java/com/example/universe/simulator/common/config/CommonProperties.java
@@ -3,10 +3,8 @@ package com.example.universe.simulator.common.config;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 
 @ConfigurationProperties(prefix = "app")
-@ConstructorBinding
 @AllArgsConstructor
 @Getter
 public class CommonProperties {
