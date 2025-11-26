4e4b10fd6a2ddee7e4eca992ce668c5c73b77d9c
nkvaratskhelia <nkvar12@gmail.com>
2025-11-26 09:08:11 +0400

    updates

--- src/main/java/com/example/universe/simulator/common/config/CachingProperties.java
+++
@@ -1,6 +0,0 @@
-package com.example.universe.simulator.common.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("app.caching")
-public record CachingProperties(boolean enabled) { }
